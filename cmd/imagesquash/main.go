493172b5e7885125f779d9f11a3d091d2e229c83
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 21:39:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/imagesquash/main.go
@@ -0,0 +1,23 @@
+// cmd/imagesquash/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"imagesquash/internal/imagesquash"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := imagesquash.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
